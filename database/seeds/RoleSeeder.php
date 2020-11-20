498d366700953e4d3f1708705989032a8d724e79
Dhani <mrmdhni01@gmail.com>
2020-11-21 01:39:46 +0700

    seeder untuk spatie

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
