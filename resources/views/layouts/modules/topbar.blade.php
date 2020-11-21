bfc1d7293a69d0213b76ff1c7bd7ca30ca66025e
Dhani <mrmdhni01@gmail.com>
2020-11-21 22:16:18 +0700

    fix dropdown user jadi kekanan

--- resources/views/layouts/modules/topbar.blade.php
+++ resources/views/layouts/modules/topbar.blade.php
@@ -39,6 +39,8 @@
                 <div class="navbar-collapse collapse" id="navbarSupportedContent">
                     <!-- Right side toggle and nav items -->
                     <!-- ============================================================== -->
+                    <ul class="navbar-nav float-left mr-auto ml-3 pl-1">
+                    </ul>
                     <ul class="navbar-nav float-right">
                         <!-- ============================================================== -->
                         <!-- User profile and search -->
@@ -49,7 +51,7 @@
                                 <img src="{{asset('admin/assets/images/users/profile-pic.jpg')}}" alt="user" class="rounded-circle"
                                     width="40">
                                 <span class="ml-2 d-none d-lg-inline-block"><span>Hello,</span> <span
-                                        class="text-dark">Jason Doe</span> <i data-feather="chevron-down"
+                                        class="text-dark">{{Auth::user()->name}}</span> <i data-feather="chevron-down"
                                         class="svg-icon"></i></span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
@@ -67,9 +69,14 @@
                                         class="svg-icon mr-2 ml-1"></i>
                                     Account Setting</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i data-feather="power"
-                                        class="svg-icon mr-2 ml-1"></i>
-                                    Logout</a>
+                                <a class="dropdown-item" href="javascript:void(0)" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        <i data-feather="power" class="svg-icon mr-2 ml-1"></i>
+                                    Logout
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
                                 <div class="dropdown-divider"></div>
                                 <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-info">View
                                         Profile</a></div>
