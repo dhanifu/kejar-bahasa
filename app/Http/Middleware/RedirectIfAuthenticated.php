dffbbd398e411e886262238e1b66124c92a3a428
agungp <55535250+agungprasetio18@users.noreply.github.com>
2020-11-21 20:28:01 +0700

    login setengah jadi

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,9 +19,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect()->route('home');
+            return redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
+
     }
 }
