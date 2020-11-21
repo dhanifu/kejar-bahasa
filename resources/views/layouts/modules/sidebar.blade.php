23b4a491491a2b16cad00068d2387221f0bc1a34
agungp <55535250+agungprasetio18@users.noreply.github.com>
2020-11-21 19:03:44 +0700

    template

---
+++ resources/views/layouts/modules/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside class="left-sidebar" data-sidebarbg="skin6">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar" data-sidebarbg="skin6">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="sidebar-item selected"> <a class="sidebar-link active" href="index.html"
+                        aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                            class="hide-menu">Dashboard</span></a></li>
+                <li class="list-divider"></li>
+                <li class="nav-small-cap"><span class="hide-menu">Applications</span></li>
+
+                <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html" aria-expanded="false"><i
+                            data-feather="tag" class="feather-icon"></i><span class="hide-menu">Ticket List
+                        </span></a>
+                </li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
+                        aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
+                            class="hide-menu">Chat</span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                        aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
+                            class="hide-menu">Calendar</span></a></li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
