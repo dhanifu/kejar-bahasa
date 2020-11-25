52aa8198e55d9450fb02be29f238806b68ac6caf
Dhani <mrmdhni01@gmail.com>
2020-11-26 00:46:03 +0700

    add sidebar Category Class

--- resources/views/layouts/modules/sidebar.blade.php
+++ resources/views/layouts/modules/sidebar.blade.php
@@ -4,22 +4,31 @@
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
             <ul id="sidebarnav">
-                <li class="sidebar-item selected"> <a class="sidebar-link active" href="index.html"
-                        aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
-                            class="hide-menu">Dashboard</span></a></li>
-                <li class="list-divider"></li>
-                <li class="nav-small-cap"><span class="hide-menu">Applications</span></li>
 
-                <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html" aria-expanded="false"><i
-                            data-feather="tag" class="feather-icon"></i><span class="hide-menu">Ticket List
-                        </span></a>
+                {{-- DASHBOARD --}}
+                <li class="sidebar-item @if(request()->routeIs('admin.home')) selected @endif"> 
+                    <a class="sidebar-link @if(request()->routeIs('admin.home')) active @endif"
+                        href="{{route('admin.home')}}" aria-expanded="false">
+                        <i data-feather="home" class="feather-icon"></i>
+                        <span class="hide-menu">Dashboard</span>
+                    </a>
                 </li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
-                        aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
-                            class="hide-menu">Chat</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
-                        aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
-                            class="hide-menu">Calendar</span></a></li>
+
+                <li class="list-divider"></li>
+                <li class="nav-small-cap">
+                    <span class="hide-menu">Applications</span>
+                </li>
+
+                {{-- CATEGORY CLASS --}}
+                <li class="sidebar-item @if(request()->routeIs('admin.category.*')) selected @endif">
+                    <a class="sidebar-link @if(request()->routeIs('admin.category.*')) active @endif" href="{{route('admin.category.index')}}"
+                        aria-expanded="false">
+                        <i data-feather="tag" class="feather-icon"></i>
+                        <span
+                            class="hide-menu">Category Class
+                        </span>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- End Sidebar navigation -->
