799873ef5d146cf2c255815c929b61b98310909f
Dhani <mrmdhni01@gmail.com>
2020-11-26 22:06:22 +0700

    fix typo 'section' to 'session'

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -31,7 +31,7 @@
 
                 @if (session('error'))
                     <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ section('error') }}
+                        {{ session('error') }}
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
