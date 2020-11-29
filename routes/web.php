d613a6488f1f26e4f78d06dbb07b1b5759b1aa5e
Dhani <mrmdhni01@gmail.com>
2020-11-29 22:57:58 +0700

    views depan

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-// Ini buat landing page aja
-Route::get('/home', 'HomeController@index')->name('home');
-// dan seterusnya
+
+Route::get('/', 'PagesController@welcome')->name('welcome');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
 
 
 // user logged in
