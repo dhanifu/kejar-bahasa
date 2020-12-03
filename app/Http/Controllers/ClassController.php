5d7e346d5fac1ea2f62464277f3deb98f68fc393
Dhani <mrmdhni01@gmail.com>
2020-12-03 07:22:07 +0700

    pembelian (setengah jadi)

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Classs;
+use App\Module;
+use App\Purchased;
+use App\Payment;
+use Auth;
 
 class ClassController extends Controller
 {
@@ -17,5 +21,62 @@ class ClassController extends Controller
     {
         $class = Classs::where('code', $code)->first();
         return view('users.preview', compact('class'));
+    }
+
+    public function beli(Request $request, Classs $class)
+    {
+        if (Auth::check()){
+            $user_id = Auth::user()->id;
+            $kelas = Purchased::create([
+                'user_id' => $user_id,
+                'class_id' => $class->id,
+            ]);
+            if (empty($request->transfer_to) && empty($request->amount)) {
+                $payment = Payment::create([
+                    'purchased_id' => $kelas->id,
+                    'transfer_to' => '-',
+                    'amount' => 0
+                ]);
+            } else {
+                $this->validate($request, [
+                    'transfer_to' => 'required|string',
+                    'amount' => 'required|integer'
+                ]);
+                $payment = Payment::create([
+                    'purchased_id' => $kelas->id,
+                    'transfer_to' => $request->transfer_to,
+                    'amount' => $request->amount
+                ]);
+            }
+
+            if ($kelas->exists && $payment->exists) {
+                $module = Module::with('class')->orderBy('created_at', 'ASC')->first();    
+                return redirect()->route('user.class.module', [$class->code, $module->code]);
+            }else{
+                return back()->with('error', 'Kelas gagal dibeli');
+            }
+        } else {
+            return redirect()->route('login');
+        }
+    }
+    
+    public function module($class, $module)
+    {
+        if (Auth::check()){
+            $c = Classs::where('code', $class)->first();
+            $bab = Module::where('class_id', $c->id)->orderBy('created_at', 'ASC')->get();
+            $classes = Classs::where('code', $class)->with('module')->get();
+            $modul = Module::where('code', $module)->first();
+            
+            if (empty($modul)){
+                return redirect()->back();
+            }
+            return view('users.modul', compact(
+                'class', 'module',
+                'bab', 'classes', 'modul'
+            ));
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
---
+++ database/migrations/2020_12_02_230724_create_payments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('purchased_id');
+            $table->string('transfer_to');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
