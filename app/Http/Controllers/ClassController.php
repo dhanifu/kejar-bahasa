c33f04fb312d384b2d254c95f7d8d883660fe0a1
Dhani <mrmdhni01@gmail.com>
2020-12-12 06:49:15 +0700

    fungsi filter belum sempurna

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CategoryClass;
 use App\Classs;
 use App\Module;
 use App\Purchased;
@@ -11,10 +12,71 @@ use Auth;
 
 class ClassController extends Controller
 {
+    private function categories()
+    {
+        return $categories = CategoryClass::orderBy('name', 'ASC')->get();
+    }
+
     public function index()
     {
-        $classes = Classs::all();
-        return view('users.class', compact('classes'));
+        $type = request()->type;
+        $category = request()->category;
+        $keyword = request()->keyword;
+        $sort = request()->sort;
+        $classes = Classs::with('category')->distinct();
+
+        if ($category != null) {
+            $classes = $classes->where('category_id', $category);
+        }
+
+        if ($type != null && $type == 'free') {
+            $classes = $classes->where('price',0);
+        } elseif ($type != null && $type == 'paid') {
+            $classes = $classes->where('price','!=', 0);
+        }
+
+        if ($keyword != null) {
+            $classes = $classes->where('name', 'LIKE',"%{$keyword}%");
+        }
+
+        if ($sort != null && $sort == 'newest') {
+            $classes = $classes->orderBy('created_at', 'DESC');
+        } elseif ($sort != null && $sort == 'oldest') {
+            $classes = $classes->orderBy('created_at', 'ASC');
+        } elseif ($sort != null && $sort == 'highest_price') {
+            $classes = $classes->orderBy('created_at', 'DESC');
+        } elseif ($sort != null && $sort == 'lowest_price') {
+            $classes = $classes->orderBy('created_at', 'ASC');
+        }
+        
+        $classes = $classes->paginate(9);
+        $categories = $this->categories();
+
+        return view('users.class', compact(
+            'categories', 'classes'
+        ));
+    }
+
+    public function searchClass(Request $request)
+    {
+        $keyword = $request->keyword;
+        $classList = Classs::with('category')->distinct()
+                    ->where('classses.name','LIKE',"%{$request->keyword}%")
+                    ->take(5)
+                    ->get();
+        $data = [];
+
+        foreach ($classList as $class) {
+            $data[] = array(
+                'category' => $class->category->name,
+                'name' => $class->name,
+                'picture' => $class->picture,
+                'price' => $class->price,
+                'link' => route('user.class.class', $class->code)
+            );
+        }
+
+        return response()->json($data);
     }
 
     public function class($code)
