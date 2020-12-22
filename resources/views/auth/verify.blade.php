46648bf6520322a65963235ce2775b13d1e4729a
Muhammad Ramdhani <mramdhani010@gmail.com>
2020-12-22 23:51:44 +0700

    ketengahin

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Verify')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 230px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
